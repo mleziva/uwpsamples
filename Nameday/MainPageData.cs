60067bc5a1340fd892b61a21f1c6db1b42fc3c1f
Austin Mleziva <ausmle@microsoft.com>
2019-05-02 18:15:58 -0500

    Adding first data binding

---
+++ Nameday/MainPageData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameday
+{
+    public class MainPageData
+    {
+        public string Greeting { get; set; } = "Helloo WORLD!";
+    }
+}
