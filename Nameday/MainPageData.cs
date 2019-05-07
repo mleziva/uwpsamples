bfb4078c1f811c74c6daf1ab7d8c1ad65346fb53
Austin Mleziva <ausmle@microsoft.com>
2019-05-07 11:48:37 -0500

    Adding nameday model and sample data initialization

--- Nameday/MainPageData.cs
+++ Nameday/MainPageData.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Nameday
 {
     public class MainPageData
     {
         public string Greeting { get; set; } = "Helloo WORLD!";
+        public List<NameDayModel> Namedays { get; set; }
+        public MainPageData()
+        {
+            Namedays = new List<NameDayModel>();
+            for (int month = 1; month <= 12; month++)
+            {
+                Namedays.Add(new NameDayModel(month, 1, new string[] { "Adam" }));
+                Namedays.Add(new NameDayModel(month, 24, new string[] { "Eve", "Andrew" }));
+            }
+        }
     }
 }
---
+++ Nameday/NameDayModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Nameday
+{
+    public class NameDayModel
+    {
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public IEnumerable<string> Names { get; }
+        public NameDayModel(int month, int day, IEnumerable<string> names)
+        {
+            Month = month;
+            Day = day;
+            Names = names;
+        }
+        public string NamesAsString
+        {
+            get { return string.Join(", ", Names); }
+        }
+    }
+}
