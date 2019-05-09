cd8edbed3fe382e00a7b52258d1e2c69406e181f
Austin Mleziva <ausmle@microsoft.com>
2019-05-09 10:37:15 -0500

    Adding ability to add, delete and modify customers

--- WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -3,25 +3,56 @@ using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using System;
+using WiredBrainCoffee.CustomersApp.DataProvider;
+using Windows.ApplicationModel;
+using WiredBrainCoffee.CustomersApp.Model;
+using System.Linq;
 
 namespace WiredBrainCoffee.CustomersApp
 {
     public sealed partial class MainPage : Page
     {
+        private CustomerDataProvider _customerDataProvider;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_LoadedAsync;
+            App.Current.Suspending += App_Suspending;
+            _customerDataProvider = new CustomerDataProvider();
         }
 
-        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+       
+
+        private async void MainPage_LoadedAsync(object sender, RoutedEventArgs e)
         {
-            var messageDialog = new MessageDialog("Customer Added!");
-            await messageDialog.ShowAsync();
+            customerListView.Items.Clear();
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+            foreach(var cust in customers)
+            {
+                customerListView.Items.Add(cust);
+            } 
+        }
+        private async void App_Suspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await _customerDataProvider.SaveCustomersAsync(customerListView.Items.OfType<Customer>());
+            deferral.Complete();
+        }
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var customer = new Customer { FirstName = "New" };
+            customerListView.Items.Add(customer);
+            customerListView.SelectedItem = customer;
         }
 
         private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
         {
-
+            var customer = customerListView.SelectedItem as Customer;
+            if(customer != null)
+            {
+                customerListView.Items.Remove(customer);
+            }
         }
 
         private void ButtonMove_Click(object sender, RoutedEventArgs e)
@@ -30,6 +61,35 @@ namespace WiredBrainCoffee.CustomersApp
             int newCol = col == 0 ? 2 : 0;
             Grid.SetColumn(customerListGrid, newCol);
             moveSymbolIcon.Symbol = newCol == 0 ? Symbol.Forward : Symbol.Back;
+        }
+
+        private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            txtFirstName.Text = customer?.FirstName ?? "";
+            txtLastName.Text = customer?.LastName ?? "";
+            chkIsDeveloper.IsChecked = customer?.IsDeveloper;
+        }
+
+        private void TxtBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void ChkBox_IsChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void UpdateCustomer()
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            if(customer != null)
+            {
+                customer.FirstName = txtFirstName.Text;
+                customer.LastName = txtLastName.Text;
+                customer.IsDeveloper = chkIsDeveloper.IsChecked.GetValueOrDefault();
+            }
         }
     }
 }
