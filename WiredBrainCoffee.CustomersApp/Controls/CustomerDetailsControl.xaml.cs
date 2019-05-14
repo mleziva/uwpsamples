3d324b2ee3d75c24da71e3f289042d592104710a
Austin Mleziva <ausmle@microsoft.com>
2019-05-14 08:04:03 -0500

    Using data bindings to customer

--- WiredBrainCoffee.CustomersApp/Controls/CustomerDetailsControl.xaml.cs
+++ WiredBrainCoffee.CustomersApp/Controls/CustomerDetailsControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using WiredBrainCoffee.CustomersApp.Model;
 
@@ -6,43 +7,16 @@ namespace WiredBrainCoffee.CustomersApp.Controls
 {
     public sealed partial class CustomerDetailsControl : UserControl
     {
-        private Customer _customer;
-
+        public static readonly DependencyProperty CustomerProperty =
+          DependencyProperty.Register("Customer", typeof(Customer), typeof(CustomerDetailsControl), new PropertyMetadata(null));
         public CustomerDetailsControl()
         {
             this.InitializeComponent();
         }
-
         public Customer Customer
         {
-            get { return _customer; }
-            set
-            {
-                _customer = value;
-                txtFirstName.Text = _customer?.FirstName ?? "";
-                txtLastName.Text = _customer?.LastName ?? "";
-                chkIsDeveloper.IsChecked = _customer?.IsDeveloper;
-            }
-        }
-
-        private void TxtBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            UpdateCustomer();
-        }
-
-        private void ChkBox_IsChanged(object sender, RoutedEventArgs e)
-        {
-            UpdateCustomer();
-        }
-
-        private void UpdateCustomer()
-        {
-            if (Customer != null)
-            {
-                Customer.FirstName = txtFirstName.Text;
-                Customer.LastName = txtLastName.Text;
-                Customer.IsDeveloper = chkIsDeveloper.IsChecked.GetValueOrDefault();
-            }
+            get { return (Customer)GetValue(CustomerProperty); }
+            set { SetValue(CustomerProperty, value); }
         }
     }
 }
